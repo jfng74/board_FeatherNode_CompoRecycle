06a338c34ebba498b21737f170ceac54068f9be3
Jean-Francois Ng <jean.francois.ng@gmail.com>
2017-05-16 20:25:16 -0400

    Mise a jour de node_feather_compost.cpp

--- Applications/Compost_Sensors/lib/NodeFeatherCompost/node_feather_compost.cpp
+++ Applications/Compost_Sensors/lib/NodeFeatherCompost/node_feather_compost.cpp
@@ -14,20 +14,22 @@
 NodeFeatherCompost::NodeFeatherCompost(){
   htu_ok = false;
   bme_ok = false;
+
+
+  // Initialisation du port serie a 9600 baud
+  Serial1.begin(9600);
+  Serial1.println("Initialisation");
   rf95 = new RH_RF95(RFM95_CS, RFM95_INT);
+
+
 // Initialisation du RTC
 //  rtc.begin();
 //	rtc.setTime(0,0,0);
 //	rtc.setAlarmTime(0,5,0);
 
-  htu_ok = htu.begin();
-  if (!htu_ok) {
-    Serial.println("Couldn't find sensor!");
-  }
-
   bme_ok  = bme.begin(0x76);
   if (!bme_ok) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    Serial1.println("Could not find a valid BME280 sensor, check wiring!");
 //		while (1);
   }
   // Initialisation de la pin de sortie pour la led rouge et eteint la led
@@ -38,8 +40,6 @@ NodeFeatherCompost::NodeFeatherCompost(){
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
-  // Initialisation du port serie a 9600 baud
-  Serial.begin(9600);
 
   // Initialisation du voltage de reference pour le ADC
   analogReference(AR_EXTERNAL);
@@ -51,28 +51,67 @@ NodeFeatherCompost::NodeFeatherCompost(){
   delay(10);
   // Initialisation du RFM
 	while (!rf95->init()) {
-		Serial.println("LoRa radio init failed");
+		Serial1.println("LoRa radio init failed");
 		while (1);
 	}
-	Serial.println("LoRa radio init OK!");
+	Serial1.println("LoRa radio init OK!");
 
 	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
 	if (!rf95->setFrequency(RF95_FREQ)) {
-		Serial.println("setFrequency failed");
+		Serial1.println("setFrequency failed");
 		while (1);
 	}
-	Serial.print("Set Freq to: ");
-	Serial.println(RF95_FREQ);
+	Serial1.print("Set Freq to: ");
+	Serial1.println(RF95_FREQ);
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
 	// The default transmitter power is 13dBm, using PA_BOOST.
 	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
 	// you can set transmitter powers from 5 to 23 dBm:
 	rf95->setTxPower(5, false);
-	blink_led(10,100);
+  Serial1.println("Fin initialisation");
 }
 
 void NodeFeatherCompost::loop(void){
+  Serial1.println("Debut Loop");
+  ntc_1 = read_temp(NTC_1);
+  ntc_2 = read_temp(NTC_2);
+  batt_voltage = read_batt_voltage();
+  if(bme_ok){
+    bme_humidity = bme.readHumidity();
+    bme_pression = bme.readPressure();
+    bme_temp = bme.readTemperature();
+  }
+  else{
+    bme_humidity = 0;
+    bme_pression = 0;
+    bme_temp = 0;
+  }
+  conductivite = 0;
+
+  send_all_data();
+
+//  dt = clock.getDateTime();
+//  Serial.println(clock.dateFormat("d-m-Y H:i:s - l", dt));
+//  digitalWrite(alarmLED, true);
+//  delay(1000);
+//  digitalWrite(alarmLED, false);
+//  delay(1000);
+//  if (clock.isAlarm1(false)) // check if alarm 1 is set
+//  { clock.armAlarm1(true); // if yes, then arm it again for next period
+
+  Serial1.println("clock.begin");
+  clock.begin();
+  clock.setBattery(true,false);
+  clock.enableOutput(false);
+  Serial1.println("clock.setAlarm1");
+  clock.setAlarm1(0, 0, 0, 5, DS3231_MATCH_S,true);
+//  clock.armAlarm1(true);
+//  clock.clearAlarm1();
+//  }
+
+
+/*
   if (rf95->available()) {
     if (rf95->recv(buf, &len)) {
       RH_RF95::printBuffer("Received: ", buf, len);
@@ -86,6 +125,8 @@ void NodeFeatherCompost::loop(void){
       Serial.println("Receive failed");
     }
   }
+  */
+   Serial1.println("Fin Loop");
 }
 
 void NodeFeatherCompost::send_batt_voltage(void){
@@ -112,42 +153,26 @@ void NodeFeatherCompost::send_batt_voltage(void){
 
 }
 
-void NodeFeatherCompost::send_all(void){
-  float t_1, t_2, t_3, h_1, batt_voltage;
+void NodeFeatherCompost::send_all_data(void){
+	byte* float_array_ntc_1;
+	float_array_ntc_1 = (byte*) &ntc_1;
 
-	// Lecture de la temperature de surface
-	t_1 = read_temp(THERMISTORPIN_A0);
-	byte* float_array_t_1;
-	float_array_t_1 = (byte*) &t_1;
+  byte* float_array_ntc_2;
+	float_array_ntc_2 = (byte*) &ntc_2;
 
-	// Lecture de la temperature de profondeur
-	t_2 = read_temp(THERMISTORPIN_A1);
-	byte* float_array_t_2;
-	float_array_t_2 = (byte*) &t_2;
+  byte* float_array_bme_humidity;
+	float_array_bme_humidity = (byte*) &bme_humidity;
 
-	// Lecture de la temperature ambiante
-	if(htu_ok)
-		t_3 = htu.readTemperature();
-	else if(bme_ok)
-		t_3 = bme.readTemperature();
-	else
-		t_3 = 0;
-	byte* float_array_t_3;
-	float_array_t_3 = (byte*) &t_3;
+  byte* float_array_bme_pression;
+	float_array_bme_pression = (byte*) &bme_pression;
 
-	// Lecture de l'humidite ambiante
-	if(htu_ok)
-		h_1 = htu.readHumidity();
-	else if(bme_ok)
-		h_1 = bme.readHumidity();
-	else
-		h_1 = 0.0;
-	byte* float_array_h_1;
-	float_array_h_1 = (byte*) &h_1;
+  byte* float_array_bme_temp;
+	float_array_bme_temp = (byte*) &bme_temp;
 
-	// Lecture du voltage de la batterie
-	batt_voltage = read_batt_voltage();
-	byte* float_array_batt_voltage;
+  byte* float_array_conductivite;
+	float_array_conductivite = (byte*) &conductivite;
+
+  byte* float_array_batt_voltage;
 	float_array_batt_voltage = (byte*) &batt_voltage;
 
 	radiopacket[0]=FEATHER_MSG_HEADER;
@@ -155,29 +180,29 @@ void NodeFeatherCompost::send_all(void){
 	radiopacket[2]=NODE_ADDR;
 	radiopacket[3]=READ_ALL_DATA;
 
-	// t_1
-	radiopacket[4]= float_array_t_1[3];
-	radiopacket[5]= float_array_t_1[2];
-	radiopacket[6]= float_array_t_1[1];
-	radiopacket[7]= float_array_t_1[0];
+	// NTC_1
+	radiopacket[4]= float_array_ntc_1[3];
+	radiopacket[5]= float_array_ntc_1[2];
+	radiopacket[6]= float_array_ntc_1[1];
+	radiopacket[7]= float_array_ntc_1[0];
 
-	// t_2
-	radiopacket[8]= float_array_t_2[3];
-	radiopacket[9]= float_array_t_2[2];
-	radiopacket[10]= float_array_t_2[1];
-	radiopacket[11]= float_array_t_2[0];
+	// NTC_2
+	radiopacket[8]= float_array_ntc_2[3];
+	radiopacket[9]= float_array_ntc_2[2];
+	radiopacket[10]= float_array_ntc_2[1];
+	radiopacket[11]= float_array_ntc_2[0];
 
-	// t_3
-	radiopacket[12]= float_array_t_3[3];
-	radiopacket[13]= float_array_t_3[2];
-	radiopacket[14]= float_array_t_3[1];
-	radiopacket[15]= float_array_t_3[0];
+	// BME_TEMP
+	radiopacket[12]= float_array_bme_temp[3];
+	radiopacket[13]= float_array_bme_temp[2];
+	radiopacket[14]= float_array_bme_temp[1];
+	radiopacket[15]= float_array_bme_temp[0];
 
-	// h_1
-	radiopacket[16]= float_array_h_1[3];
-	radiopacket[17]= float_array_h_1[2];
-	radiopacket[18]= float_array_h_1[1];
-	radiopacket[19]= float_array_h_1[0];
+	// BME_HUMIDITY
+	radiopacket[16]= float_array_bme_humidity[3];
+	radiopacket[17]= float_array_bme_humidity[2];
+	radiopacket[18]= float_array_bme_humidity[1];
+	radiopacket[19]= float_array_bme_humidity[0];
 
 	// batt_voltage
 	radiopacket[20]= float_array_batt_voltage[3];
@@ -185,7 +210,18 @@ void NodeFeatherCompost::send_all(void){
 	radiopacket[22]= float_array_batt_voltage[1];
 	radiopacket[23]= float_array_batt_voltage[0];
 
-	radiopacket[24]= FEATHER_MSG_END;
+  radiopacket[24]= float_array_bme_pression[3];
+	radiopacket[25]= float_array_bme_pression[2];
+	radiopacket[26]= float_array_bme_pression[1];
+	radiopacket[27]= float_array_bme_pression[0];
+
+  radiopacket[28]= float_array_conductivite[3];
+	radiopacket[29]= float_array_conductivite[2];
+	radiopacket[30]= float_array_conductivite[1];
+	radiopacket[31]= float_array_conductivite[0];
+
+
+	radiopacket[32]= FEATHER_MSG_END;
 
 	rf95->send((uint8_t *)radiopacket, sizeof(radiopacket));
 	rf95->waitPacketSent();
@@ -279,8 +315,8 @@ void NodeFeatherCompost::blink_led(uint8_t nb_flash, uint32_t delais){
 
 void NodeFeatherCompost::parse_data(uint8_t Thebuf[]){
 	if(Thebuf[0]==FEATHER_MSG_HEADER && Thebuf[1]==FEATHER_MSG_GET_ALL_DATA && Thebuf[2]==NODE_ADDR && Thebuf[3]==READ_ALL_DATA) {
-		Serial.println("sendAll");
-		send_all();
+		Serial1.println("sendAll");
+		send_all_data();
 	    //blink_led(2,100);
 	    blink_led(2,100);
 	}
@@ -298,11 +334,11 @@ void NodeFeatherCompost::parse_data(uint8_t Thebuf[]){
 
 	else if (Thebuf[0]==FEATHER_MSG_HEADER && Thebuf[1]==FEATHER_MSG_QUERY_DATA && Thebuf[2]==NODE_ADDR && Thebuf[3]==TEMP_1) {
 		send_temp(THERMISTORPIN_A0);
-		Serial.println("Send temp 1");
+		Serial1.println("Send temp 1");
 
     }
 	else if (Thebuf[0]==FEATHER_MSG_HEADER && Thebuf[1]==FEATHER_MSG_QUERY_DATA && Thebuf[2]==NODE_ADDR && Thebuf[3]==TEMP_2) {
 		send_temp(THERMISTORPIN_A1);
-		Serial.println("Send temp 2");
+		Serial1.println("Send temp 2");
     }
  }
